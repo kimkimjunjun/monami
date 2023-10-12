65d519f6f6d7e22e0650b76e8497b1126a99f9e0
kimkimjunjun <wnsgnl323@gmail.com>
2023-10-12 19:37:50 +0900

    헤더 수정

--- components/header.tsx
+++ components/header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
                 />
                 <div>
                     <span className={`cursor-pointer hover:text-[#c40f39]`} onMouseEnter={() => handleMouseEnter("introduction")} onMouseLeave={() => handleMouseLeave("introduction")}>모나미소개</span>
-                    <div className={`${isOpen.introduction ? "block transition-div" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 opacity-90`}>
+                    <div className={`${isOpen.introduction ? "block transition-div" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 bg-white opacity-90`}>
                         <span className="mt-3">CEO 메시지</span>
                         <hr />
                         <span>회사정보</span>
@@ -51,7 +51,7 @@ export default function Header() {
                 </div>
                 <div>
                     <span className="cursor-pointer hover:text-[#c40f39]" onMouseEnter={() => handleMouseEnter("products")} onMouseLeave={() => handleMouseLeave("products")}>모나미제품</span>
-                    <div className={`${isOpen.products ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 opacity-90`}>
+                    <div className={`${isOpen.products ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 bg-white opacity-90`}>
                         <span className="mt-3">펜</span>
                         <hr />
                         <span>마카</span>
@@ -67,7 +67,7 @@ export default function Header() {
                 </div>
                 <div>
                     <span className="cursor-pointer hover:text-[#c40f39]" onMouseEnter={() => handleMouseEnter("news")} onMouseLeave={() => handleMouseLeave("news")}>NEWS&VIDEO</span>
-                    <div className={`${isOpen.news ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 opacity-90`}>
+                    <div className={`${isOpen.news ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 bg-white opacity-90`}>
                         <span className="mt-3">보도자료</span>
                         <hr />
                         <span className="pb-4">동영상자료</span>
@@ -75,7 +75,7 @@ export default function Header() {
                 </div>
                 <div>
                     <span className="cursor-pointer hover:text-[#c40f39]" onMouseEnter={() => handleMouseEnter("competition")} onMouseLeave={() => handleMouseLeave("competition")}>모나미 미술대회</span>
-                    <div className={`${isOpen.competition ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 opacity-90`}>
+                    <div className={`${isOpen.competition ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 bg-white opacity-90`}>
                         <span className="mt-3">미술대회 소개</span>
                         <hr />
                         <span className="pb-4">수상작 발표</span>
@@ -83,7 +83,7 @@ export default function Header() {
                 </div>
                 <div>
                     <span className="cursor-pointer hover:text-[#c40f39]" onMouseEnter={() => handleMouseEnter("support")} onMouseLeave={() => handleMouseLeave("support")}>고객지원</span>
-                    <div className={`${isOpen.support ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 opacity-90`}>
+                    <div className={`${isOpen.support ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 bg-white opacity-90`}>
                         <span className="mt-3">자주묻는질문</span>
                         <hr />
                         <span>문의하기</span>
@@ -93,7 +93,7 @@ export default function Header() {
                 </div>
                 <div>
                     <span className="cursor-pointer hover:text-[#c40f39]" onMouseEnter={() => handleMouseEnter("purchase")} onMouseLeave={() => handleMouseLeave("purchase")}>기업 구매/개발</span>
-                    <div className={`${isOpen.purchase ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 opacity-90`}>
+                    <div className={`${isOpen.purchase ? "block" : "hidden"} absolute -ml-16 top-20 w-52 flex flex-col px-5 shadow-md text-center space-y-3 bg-white opacity-90`}>
                         <span className="mt-3">대량 구매 문의</span>
                         <hr />
                         <span>기업 및 단체 구매</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/header";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      ddd
+      <img
+        src="http://www.monami.com/images/main/visual1.jpg"
+        alt="이미지"
+      />
     </div>
   )
 }
